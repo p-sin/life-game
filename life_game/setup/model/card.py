1d9967fc20f539dd093b876fcbaaefb56f4a9686
Philip <p-sinfield@live.co.uk>
2023-09-09 19:46:11 +0100

    Remove redundant files superceded by Card and Event classes

--- life_game/setup/model/card.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-from life_game.setup.model.card_stat import CardStat
-
-
-class Card(BaseModel):
-    section_1: CardStat
-    section_2: CardStat
-    section_3: CardStat
--- life_game/setup/model/card_stat.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-
-class CardStat(BaseModel):
-    board_section: int
-    attribute: str
-    value: int
